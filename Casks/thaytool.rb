aa992c79a45e3e985b61cce0494e81a8a0ed4a92
Rafael Thayto <rafa.thayto@gmail.com>
2026-09-03 09:29:24 -0300

    thaytool 0.83.0

--- Casks/thaytool.rb
+++ Casks/thaytool.rb
@@ -1,6 +1,6 @@
 cask "thaytool" do
-  version "0.82.1"
-  sha256 "b6a7f3ac9f3537ba7633095e3fb85165de1177613012e5063720fdc8ecc705ae"
+  version "0.83.0"
+  sha256 "543939976ec25bb8aa5e62d3319f89bfb06e1d4c73efc7e008fe7bb16a08c319"
 
   url "https://assets.thaytool.com/releases/stable/v#{version}/Thaytool.dmg"
   name "Thaytool"
